bea2164cd4dbf68a0cadeb40375d2dba175040dc
Mohil Thummar <sq.mohil@gmail.com>
2023-03-01 10:29:50 +0530

    test

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,9 @@ import 'package:firebase_chat/utils/app_string.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
+///
+///
+///
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
